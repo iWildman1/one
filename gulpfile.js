95f22eadacf08ba7d257cd7af1409826f1dd4552
Daniel Wildman <iwildman1@hotmail.com>
2017-03-27 16:41:46 +0100

    Added add/edit project templates, and fixed sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var wait = require('gulp-wait');
 var browserSync = require('browser-sync');
 
 gulp.task('browser-sync', ['sass'], function() {
@@ -18,6 +19,7 @@ gulp.task('watch', ['browser-sync'], function() {
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/*.scss')
+        .pipe(wait(100))
         .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
